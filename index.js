229ff32284773fa9f46a461aa2cacbac3df1e949
demeulenaeresam <sam.demeulenaere@student.howest.be>
2017-05-17 19:53:12 +0200

    start

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
-server.listen(5000);
+server.listen(process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
